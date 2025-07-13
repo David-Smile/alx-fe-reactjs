eb72c9499f02027e369873be04b19d3b73ee782e
David-Smile <orjidavid18@gmail.com>
2025-07-13 20:11:57 +0100

    Creating a simple profile cards with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import Header from './components/Header.jsx'
-import MainContent from './components/MainContent.jsx'
-import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile name="John" age={25} bio="I am a software engineer" />
     </div>
   )
 }
