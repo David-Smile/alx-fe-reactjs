dc1dd151fad30b6e5911553992df62054fd06c59
David-Smile <orjidavid18@gmail.com>
2025-08-06 13:05:18 +0100

    added responsive typography

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
       <div className="sm:p-4 md:p-8 bg-gray-100 p-8 sm:max-w-xs md:max-w-sm mx-auto, my-20 rounded-lg shadow-lg user-profile">
         <img className="rounded-full sm:w-24 h-24 md:w-36 h-36 mx-auto outline-2 outline-offset-2 outline-blue-800 user-image"src="https://via.placeholder.com/150" alt="User" />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 user-name">John Doe</h1>
-        <p classNamw="text-gray-600 text-base user-descritpion">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <p className="text-gray-600 sm:text-sm md:text-base user-descritpion">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
