4cb8303a4e49821242c8e9ff9362efe994f84b13
David-Smile <orjidavid18@gmail.com>
2025-08-24 01:40:16 +0100

    Advanced Data Handling with React Query

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import PostsComponent from './PostsComponent.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <PostsComponent />
+    </div>
+  )
+}
+
+export default App
