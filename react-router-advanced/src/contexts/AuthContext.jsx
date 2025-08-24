91ea02733b40838dc70f8d5ed83f39c0c39b62d7
David-Smile <orjidavid18@gmail.com>
2025-08-24 16:10:38 +0100

    React Router setup in main component

--- react-router-advanced/src/contexts/AuthContext.jsx
+++ react-router-advanced/src/contexts/AuthContext.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { AuthContext } from './AuthContextBase';
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
