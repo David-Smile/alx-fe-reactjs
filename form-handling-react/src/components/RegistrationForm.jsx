b2c59437a5fcd259f3551f19fd61d3ac9de740ba
David-Smile <orjidavid18@gmail.com>
2025-08-24 01:17:58 +0100

    Controlled components setup

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function RegistrationForm() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-        <h2>Registration Form</h2>
-        <form>
-            <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required />
-            </div>
-            <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-            </div>
-            <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Register</button>
-        </form>
+      <h2>Registration Form</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
   )
 }
